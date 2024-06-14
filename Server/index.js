dbd6c25c1c0a7fa380b1953bf47abe56a184d951
123AbhiRaghav <abhiraghavd@gmail.com>
2024-06-14 11:50:33 +0530

    update

--- Server/index.js
+++ Server/index.js
@@ -5,21 +5,18 @@ import cors from "cors";
 import morgan from "morgan";
 import dbConnection from "./utils/index1.js";
 import { errorHandler,routeNotFound } from "./middleware/error.js";
-import router from './routes/route.js'
-
-const routes = "";
+import routes from "./routes/route.js"
 
 dotenv.config();
 
 dbConnection();
 
 const PORT = process.env.PORT || 3000;
-const HOSTNAME = process.env.HOSTNAME;
 
 const app = express();
 
 app.use(cors({
-   origin: ["https://client1-t24j.vercel.app/log-in"],
+   origin: ["http://localhost:3000", "http://localhost:3001"],
    methods: ["GET", "POST", "PUT", "DELETE"],
    credentials: true,
 }));
@@ -28,7 +25,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 app.use(morgan("dev"))
-app.use("/api", router)
+app.use("/api", routes)
 app.use(routeNotFound)
 app.use(errorHandler)
 
